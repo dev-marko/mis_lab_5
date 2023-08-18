0aa471c4f00b9198b7090f5ee5fb2ab7b29b20fc
dev-marko <markospasenovski00@gmail.com>
2023-08-18 23:50:46 +0200

    Add google maps keys to android and ios builds

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyDoJ7FoeZbiGOTYG-VyFOnaNELPCaJYgaQ")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
