b93666a0042abb625cd0ce9dcf8ed321f3f19659
dev-marko <markospasenovski00@gmail.com>
2023-08-18 23:56:22 +0200

    Update exam model

--- lib/models/exam.dart
+++ lib/models/exam.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mis_lab_4/models/location.dart';
 
 class Exam with ChangeNotifier {
   final String? id;
   final String subjectName;
   final DateTime date;
+  final Location? location;
 
   Exam({
     this.id,
     required this.subjectName,
     required this.date,
+    this.location,
   });
 
   @override
